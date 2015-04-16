1c7ec3cf4d2118b65fcfda07e97097d652568610
Hikaru <hikaru.ito.1995@gmail.com>
2015-04-16 15:42:42 +0900

    InAppBrowserの関数実行テスト成功

--- www/js/app.js
+++ www/js/app.js
@@ -5,12 +5,13 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
 
-.controller('PageCtrl', function($scope, $state, $stateParams, $sce, Pages) {
+.controller('PageCtrl', function($scope, $state, $stateParams, $sce, $cordovaInAppBrowser, Pages) {
   $scope.page = Pages.getPage($stateParams.pageId);
   $scope.isLoading = true;
   // ページ本文を読み込む
@@ -12,6 +12,16 @@ angular.module('starter.controllers', [])
   // htmlを埋め込むための変換関数
   $scope.getTag = function(txt) {
         return $sce.trustAsHtml(txt);
+  }
+  // InAppBrowser
+  $scope.openWebPage = function(url) {
+   var options = {
+        location: 'yes',
+        clearcache: 'yes',
+        toolbar: 'no'
+      };
+    $cordovaInAppBrowser.open(url, '_blank', options)
+      .then(function(event) {}).catch(function(event) {});
   }
 })
 
